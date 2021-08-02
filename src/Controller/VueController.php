3928c6cbfa37b3a630d3166d9238e36778bb03ae
mbrignon <mbrignon@osmozis.com>
2021-08-02 11:31:03 +0200

    login page style

--- src/Controller/VueController.php
+++ src/Controller/VueController.php
@@ -15,7 +15,7 @@ class VueController extends AbstractController
     {
         return $this->render('vue/index.html.twig', [
             'title' => 'BookWatt',
-            'main_technos' => ['Symfony 5.3', 'Api-platform 2.3', 'Vuejs 2.6', 'Bootstrap-vue 2.21 (Bootstrap 5.0)', 'Docker (Php 8.0 / Nginx 1.21 / Posgresql 13.3)']
+            'main_technos' => ['Symfony 5.3', 'Api-platform 2.3', 'Easyadmin 3.4', 'Vuejs 2.6', 'Bootstrap-vue 2.21 (Bootstrap 5.0)', 'Docker (Php 8.0 / Nginx 1.21 / Posgresql 13.3)']
         ]);
     }
 }
